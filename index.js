41cafd2fa6e3dfd1f13f23dfc9540da250f9052c
Scottie Schneider <scott@laclave.co>
2017-09-10 15:51:43 -0500

    Commit 3 ✅ Added input, form, file picker for video. Working on finding the length of the video itself.

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const electron = require('electron');
 // ES6 destructuring below, need to research more on this
 const { app, BrowserWindow } = electron;
 app.on('ready', () => {
-	new BrowserWindow({});
+	const mainWindow = new BrowserWindow({});
+	mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
