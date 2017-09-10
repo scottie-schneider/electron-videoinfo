1adf720bbc96b41da5c4cb5034ad93e9d4ce594e
Scottie Schneider <scott@laclave.co>
2017-09-10 17:04:44 -0500

    Commit 5 - It's alive! App complete, at least in functionality.

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
 
 // ES6 destructuring below, need to research more on this
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
+let mainWindow;
+
 app.on('ready', () => {
-	const mainWindow = new BrowserWindow({});
+	mainWindow = new BrowserWindow({});
 	mainWindow.loadURL(`file://${__dirname}/index.html`);
+});
+// IPC is pretty confusing, check out this flow of the different objects:
+// https://screencast.com/t/39NKGCBwbR
+ipcMain.on('video:submit', (event, path) => {
+	ffmpeg.ffprobe(path, (err, metadata) => {
+		mainWindow.webContents.send('video:metadata', metadata.format.duration);
+	});
 });
